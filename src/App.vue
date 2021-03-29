70a7d1ea0c7c0cf4fdc3fe784f68e475644e52ae
Masagus Hariadi Arief <adi.arief77@gmail.com>
2021-03-29 21:53:53 +0700

    add: pokemon

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Pokemon/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pokemon from './components/Pokemon'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Pokemon
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
